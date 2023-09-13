4766a43c326a02180b6fa5e51442c9496d8daa05
ChinhLT <chinhlt@hybrid-technologies.vn>
2023-09-13 22:22:25 +0700

    feat: update code base

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,17 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { UserModule } from './modules/user/user.module';
+import { DatabaseModule } from './utils/database';
+import { AuthModule } from './modules/auth/auth.module';
 
 @Module({
-  imports: [UserModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    DatabaseModule,
+    UserModule,
+    AuthModule,
+  ],
 })
 export class AppModule {}
--- src/modules/user/entities/user.entity.ts
+++ src/modules/user/entities/user.entity.ts
@@ -1,1 +1,31 @@
-export class User {}
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class User {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column({ unique: true })
+  username: string;
+
+  @Column({ unique: true })
+  email: string;
+
+  @Column()
+  password: string;
+
+  @Column()
+  avatar: string;
+
+  @Column()
+  birthday: Date;
+
+  @Column()
+  name: string;
+
+  @Column()
+  phone: string;
+
+  @Column()
+  address: string;
+}
